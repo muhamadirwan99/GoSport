188e1d9f85b657ec2cc35f9d0decc4fbde5a5132
Yuummn <yamincode@gmail.com>
2021-12-01 18:33:19 +0700

    mengubah beberapa file

--- lib/common/style.dart
+++ lib/common/style.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 const Color secondaryColor = Color(0xff95fe84);
 double defaultMargin = 30.0;
@@ -8,3 +9,30 @@ FontWeight medium = FontWeight.w500;
 FontWeight semiBold = FontWeight.w600;
 FontWeight bold = FontWeight.bold;
 TextStyle blur = TextStyle(color: Color.fromARGB(80, 0, 0, 0));
+
+final TextTheme myTextTheme = TextTheme(
+  headline1: GoogleFonts.montserrat(
+      fontSize: 82, fontWeight: FontWeight.w300, letterSpacing: -1.5),
+  headline2: GoogleFonts.montserrat(
+      fontSize: 51, fontWeight: FontWeight.w300, letterSpacing: -0.5),
+  headline3: GoogleFonts.montserrat(fontSize: 41, fontWeight: FontWeight.w400),
+  headline4: GoogleFonts.montserrat(
+      fontSize: 29, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+  headline5: GoogleFonts.montserrat(fontSize: 21, fontWeight: FontWeight.w400),
+  headline6: GoogleFonts.montserrat(
+      fontSize: 17, fontWeight: FontWeight.w500, letterSpacing: 0.15),
+  subtitle1: GoogleFonts.montserrat(
+      fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.15),
+  subtitle2: GoogleFonts.montserrat(
+      fontSize: 12, fontWeight: FontWeight.w500, letterSpacing: 0.1),
+  bodyText1: GoogleFonts.montserrat(
+      fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.5),
+  bodyText2: GoogleFonts.montserrat(
+      fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+  button: GoogleFonts.montserrat(
+      fontSize: 12, fontWeight: FontWeight.w500, letterSpacing: 1.25),
+  caption: GoogleFonts.montserrat(
+      fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 0.4),
+  overline: GoogleFonts.montserrat(
+      fontSize: 9, fontWeight: FontWeight.w400, letterSpacing: 1.5),
+);
